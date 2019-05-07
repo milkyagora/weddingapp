c382bee8e8287d94f11be0d8f0da3e7ce854f318
milky <milky.agora@boxchain.com>
2019-05-07 12:51:48 +0800

    uploaded xcode project

---
+++ WeddingApp/WeddingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeddingApp
+//
+//  Created by Milky Joy Agora on 07/05/2019.
+//  Copyright © 2019 Milky Joy Agora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WeddingApp/WeddingAppTests/WeddingAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeddingAppTests.swift
+//  WeddingAppTests
+//
+//  Created by Milky Joy Agora on 07/05/2019.
+//  Copyright © 2019 Milky Joy Agora. All rights reserved.
+//
+
+import XCTest
+@testable import WeddingApp
+
+class WeddingAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
